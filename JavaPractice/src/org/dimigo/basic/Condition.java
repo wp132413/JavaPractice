799599c1c3fc76fcb3c21dd2309bec917c8c64c1
Jun Ho <Jun Ho@PJH>
2015-03-26 21:43:51 +0900

    네번째 실습과제(수정)

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -3,30 +3,34 @@ package org.dimigo.basic;
 public class Condition {
 
 	public static void main(String[] args) {
-		int distance = 10;
-		int money = 0;
+		int distance = 31;
+		int dis;
+		int pay;
+		int plus;
+		int total;
 		String car = "고속버스";
 		
+		dis = (distance-1)/10;
+		
 		if(car == "고속버스"){
-			if(distance <= 10) money = 850;
-			else if(distance > 10 && distance <= 20) money = 850 + 300;
-			else if(distance > 20 && distance <= 30) money = 850 + 600;
+			pay = 850;
+			plus = 300;
 		}
 		else if(car == "경차"){
-			if(distance <= 10) money = 300;
-			else if(distance > 10 && distance <= 20) money = 300 + 200;
-			else if(distance > 20 && distance <= 30) money = 300 + 400;
+			pay = 300;
+			plus = 200;
 		}
 		else{
-			if(distance <= 10) money = 600;
-			else if(distance > 10 && distance <= 20) money = 600 + 200;
-			else if(distance > 20 && distance <= 30) money = 600 + 400;
+			pay = 600;
+			plus = 200;
 		}
 		
-		System.out.println("<< 고속도로 통행료 계산 >>");
-		System.out.println("거리 : " + distance + "km");
+		total = pay + plus * dis;
+		
+		System.out.println("<<<고속도로 통행료 계산>>>");
+		System.out.println("거리 : " +distance +"km");
 		System.out.println("차종 : " + car);
-		System.out.println("통행료 : " + money + "원");
+		System.out.println("통행료 : " + total +"원");
 
 	}
 
