1725e7957be003bd99cf8cc23df021db3f56fc59
Jun Ho <Jun Ho@PJH>
2015-11-06 11:47:00 +0900

    rwe

---
+++ JavaPractice/src/org/dimigo/thread/Race2.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ *   ㅣ_ Race
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 11. 4.
+ * </pre>
+ *
+ * @author  : 박준호
+ * @version : 1.0
+ */
+public class Race2 {
+	public static void main(String[] args) { 
+		System.out.println("main thread start"); 
+ 		 
+ 		Thread t1 = new Runner("정욱재"); 
+ 		Thread t2 = new Runner("방현우"); 
+ 		
+ 		t2.setPriority(Thread.MAX_PRIORITY); 
+ 		t1.setPriority(Thread.MIN_PRIORITY); 
+ 		 
+ 		t1.start(); 
+ 		t2.start(); 
+ 		 
+ 		System.out.println("main thread end"); 
+ 	} 
+}
